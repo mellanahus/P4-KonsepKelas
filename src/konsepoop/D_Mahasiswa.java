8f7ab2ed9d7787ec71627f5ca60ccfe69632201c
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-15 11:27:41 +0700

    Percobaan 4 Percobaan untuk menjalankan kelas A_Mahasiswa

---
+++ src/konsepoop/D_Mahasiswa.java
@@ -0,0 +1,38 @@
+// Percobaan untuk menjalankan kelas A_Mahasiswa
+
+package konsepoop;
+
+public class D_Mahasiswa {
+    private String nama;
+    private int nrp;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getNrp() {
+        return nrp;
+    }
+
+    public void setNrp(int nrp) {
+        this.nrp = nrp;
+    }
+
+    public D_Mahasiswa(String nama, int nrp) {
+        this.nama = nama;
+        this.nrp = nrp;
+    }
+
+    public void info() {
+        System.out.println(nama + ", " + nrp);
+    }
+
+    public String toString() {
+        System.out.println("Data Mahasiswa\n" + nama + "\n" + nrp);
+        return null;
+    }
+}
