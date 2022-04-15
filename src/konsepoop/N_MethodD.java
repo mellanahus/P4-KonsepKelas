50220bfcbd50ece39925ebdce6dbd5cefc039799
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-15 11:51:51 +0700

    Percobaan 14 Percobaan menggunakan metode dengan nilai kembali yang memiliki parameter

--- src/konsepoop/N_MethodD.java
+++ src/konsepoop/N_MethodD.java
@@ -4,12 +4,12 @@ yang memiliki parameter */
 package konsepoop;
 
 public class N_MethodD {
-    public static void main(String[] args) {
-        double hasilBagi = bagi(9, 3);
-        System.out.println("Hasil bagi = " + hasilBagi);
-    }
+        public static void main(String[] args) {
+            double hasilBagi = bagi(9, 3);
+            System.out.println("Hasil bagi = " + hasilBagi);
+        }
 
-    private static double bagi(double param1, double param2) {
-        return (param1 / param2);
+        private static double bagi(double param1, double param2) {
+            return (param1 / param2);
+        }
     }
-}
