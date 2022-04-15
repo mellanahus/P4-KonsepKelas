f060c646bd820dfc2458acd056fb718039bf4aa8
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-15 11:43:45 +0700

    Percobaan 11 Percobaan menggunakan metode tanpa nilai kembali (void)

---
+++ src/konsepoop/K_MethodA.java
@@ -0,0 +1,13 @@
+// Percobaan menggunakan metode tanpa nilai kembali (void)
+
+package konsepoop;
+
+public class K_MethodA {
+    public static void main(String[] args) {
+        welcome();
+    }
+
+    private static void welcome() {
+        System.out.println("Selamat Belajar Java");
+    }
+}
