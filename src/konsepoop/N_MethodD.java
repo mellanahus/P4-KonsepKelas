f3a4365c9eb5d3a8197b7eaead6007e9585dfe9b
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-15 11:48:09 +0700

    Percobaan 13 Percobaan menggunakan method tanpa nilai kembali yang memiliki parameter

---
+++ src/konsepoop/N_MethodD.java
@@ -0,0 +1,15 @@
+/* Percobaan menggunakan metode dengan nilai kembali
+yang memiliki parameter */
+
+package konsepoop;
+
+public class N_MethodD {
+    public static void main(String[] args) {
+        double hasilBagi = bagi(9, 3);
+        System.out.println("Hasil bagi = " + hasilBagi);
+    }
+
+    private static double bagi(double param1, double param2) {
+        return (param1 / param2);
+    }
+}
