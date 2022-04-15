b88149b7a372e7e3c643ec659876adf038e1ed47
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-15 11:34:40 +0700

    Percobaan 7 Percobaan mengakses member class yang di beri akses modifier dari package yang berbeda

---
+++ src/paketA/G_Mobil.java
@@ -0,0 +1,28 @@
+package paketA;
+
+public class G_Mobil {
+    private String nama;
+    private String noBM;
+    public int jmlRoda;
+    public String bahanBakar;
+    public boolean fullAC;
+
+    public G_Mobil(String nama, String noBM) {
+        this.nama = nama;
+        this.noBM = noBM;
+        this.jmlRoda = 4;
+        this.bahanBakar  = "Bensin";
+        this.fullAC = true;
+    }
+
+    public String getInfo() {
+    String info = "Mobil : " + nama +
+    "\nNomor BM : " + noBM +
+    "\nJumlah Roda : " + jmlRoda +
+    "\nJenis Bahan Bakar : " + bahanBakar +
+    "\nFull AC : " + fullAC;
+    return info;
+    }
+}
+
+
